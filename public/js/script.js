1b54477852ecfbbd60840f32bdc1aba40309d040
Eduard Budacu <eduard.budacu@gmail.com>
2018-12-06 18:07:14 +0000

    solved bug where a duplicate request was made

--- public/js/script.js
+++ public/js/script.js
@@ -4,15 +4,13 @@ $(document).ready(function(){
 })
 
 function showCategories() {
-    $.get('/categories', function(){
-        $.get( "/categories", function( data ) {
-            var html = ''
-            data.forEach(function(category) {
-                html = html + '<li><a href="#" onClick="showProducts('+category.id+')">'+category.name+'</a></li>'
-            })
-            $('#categories').html(html)
-        });
-    })
+    $.get( "/categories", function( data ) {
+        var html = ''
+        data.forEach(function(category) {
+            html = html + '<li><a href="#" onClick="showProducts('+category.id+')">'+category.name+'</a></li>'
+        })
+        $('#categories').html(html)
+    });
 }
 
 //todo: implement showProducts method
